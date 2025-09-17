36bcef317eee0dd67ae9ecca961b4b9c5c3cf090
Cecílio Neto <dev_cnm@hotmail.com>
2025-09-17 14:59:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let btnNext = document.querySelector('.next');
+let btnBack = document.querySelector('.back'); 
+
+let container = document.querySelector('.container'); 
+let list = document.querySelector('.list');
+let thumb = document.querySelector('.thumb');
+
+btnNext.onclick = () => moverOsItensQuandoClicarNoBotao('next');
+btnBack.onclick = () => moverOsItensQuandoClicarNoBotao('back');
+
+function moverOsItensQuandoClicarNoBotao(type){
+
+    let listItems = document.querySelectorAll('.list-item');
+    let thumbItems = document.querySelectorAll('.thumb-item');
+
+    if( type === 'next') {
+       list.appendChild(listItems[0]);
+       thumb.appendChild(thumbItems[0]);
+
+       container.classList.add('next');
+ 
+    } else {
+        list.prepend(listItems[listItems.length -1])
+        thumb.prepend(thumbItems[thumbItems.length -1]);
+
+        container.classList.add('back');
+    }
+    
+    setTimeout(() => {
+         container.classList.remove('next');
+         container.classList.remove('back');
+    }, 2000);
+}
